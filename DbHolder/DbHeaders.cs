7d34028798a8e6d284c8734ba6b1b0965eab8738
Michael <michspar@mail.ru>
2019-08-05 00:27:09 +0300

    more faster (pre-unsafe) db load

--- DbHolder/DbHeaders.cs
+++ DbHolder/DbHeaders.cs
@@ -1,112 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DbHolder
 {
-    //60 bytes
-    public class DbHeader
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct DbHeader
     {
-        public int Version { get; set; }           // версия база данных
+        public int version;           // версия база данных
 
-        public string Name { get; set; }          // название/префикс для базы данных sbyte[32]
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string name;          // название/префикс для базы данных sbyte[32]
 
-        public ulong Timestamp { get; set; }         // время создания базы данных
-
-        public int Records { get; set; }           // общее количество записей
-
-        public uint Offset_ranges { get; set; }     // смещение относительно начала файла до начала списка записей с геоинформацией
-
-        public uint Offset_cities { get; set; }     // смещение относительно начала файла до начала индекса с сортировкой по названию городов
-
-        public uint Offset_locations { get; set; }  // смещение относительно начала файла до начала списка записей о местоположении
-
-        public DbHeader(byte[] buf)
-        {
-            using (var initStream = new BinaryReader(new MemoryStream(buf)))
-            {
-                Version = initStream.ReadInt32();
-                Name = initStream.ReadFixedString(32);
-                Timestamp = initStream.ReadUInt64();
-                Records = initStream.ReadInt32();
-                Offset_ranges = initStream.ReadUInt32();
-                Offset_cities = initStream.ReadUInt32();
-                Offset_locations = initStream.ReadUInt32();
-            }
-        }
+        public ulong timestamp;         // время создания базы данных
+        public int records;           // общее количество записей
+        public uint offset_ranges;     // смещение относительно начала файла до начала списка записей с геоинформацией
+        public uint offset_cities;     // смещение относительно начала файла до начала индекса с сортировкой по названию городов
+        public uint offset_locations;  // смещение относительно начала файла до начала списка записей о местоположении
     }
 
-    // 12 bytes
-    class DbRange
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct DbRange
     {
-        public const int size = 12;
-
-        public uint Ip_from { get; set; }           // начало диапазона IP адресов
-
-        public uint Ip_to { get; set; }             // конец диапазона IP адресов
-
-        public uint Location_index { get; set; }     // индекс записи о местоположении
-
-        public DbRange(byte[] buf, int index, int count)
-        {
-            using (var br = new BinaryReader(new MemoryStream(buf, index, count)))
-            {
-                Ip_from = br.ReadUInt32();
-                Ip_to = br.ReadUInt32();
-                Location_index = br.ReadUInt32();
-            }
-        }
+        public uint ip_from;           // начало диапазона IP адресов
+        public uint ip_to;             // конец диапазона IP адресов
+        public uint location_index;     // индекс записи о местоположении
     }
 
-    //96 bytes
-    class DbLocation
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct DbLocation
     {
-        public const int size = 96;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 8)]
+        public string country;        //[8] название страны (случайная строка с префиксом "cou_")
 
-        public string Country { get; set; }        //[8] название страны (случайная строка с префиксом "cou_")
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public string region;        //[12] название области (случайная строка с префиксом "reg_")
 
-        public string Region { get; set; }        //[12] название области (случайная строка с префиксом "reg_")
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 12)]
+        public string postal;        //[12] почтовый индекс (случайная строка с префиксом "pos_")
 
-        public string Postal { get; set; }        //[12] почтовый индекс (случайная строка с префиксом "pos_")
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 24)]
+        public string city;          //[24] название города (случайная строка с префиксом "cit_")
 
-        public string City { get; set; }          //[24] название города (случайная строка с префиксом "cit_")
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string organization;  //[32] название организации (случайная строка с префиксом "org_")
 
-        public string Organization { get; set; }  //[32] название организации (случайная строка с префиксом "org_")
-
-        public float latitude { get; set; }          // широта
-
-        public float longitude { get; set; }         // долгота
-
-        public DbLocation(byte[] buf, int index, int count)
-        {
-            using (var br = new BinaryReader(new MemoryStream(buf, index, count)))
-            {
-                Country = br.ReadFixedString(8);
-                Region = br.ReadFixedString(12);
-                Postal = br.ReadFixedString(12);
-                City = br.ReadFixedString(24);
-                Organization = br.ReadFixedString(32);
-                latitude = br.ReadSingle();
-                longitude = br.ReadSingle();
-            }
-        }
+        public float latitude;          // широта
+        public float longitude;         // долгота
     }
 
-    class DbCity
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct DbCity
     {
-        public int Index { get; set; }          //список индексов записей местоположения отсортированный по названию города
-
-        public DbCity(byte[] buf, int index, int count)
-        {
-            using (var br = new BinaryReader(new MemoryStream(buf, index, count)))
-            {
-                Index = br.ReadInt32();
-            }
-        }
+        public uint index;         //список индексов записей местоположения отсортированный по названию города
     }
 }
--- DbHolder/DbHolder.cs
+++ DbHolder/DbHolder.cs
@@ -2,106 +2,52 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DbHolder
 {
-    static class ExtendBinaryReader
-    {
-        public static string ReadFixedString(this BinaryReader reader, int len)
-        {
-            var buffer = new byte[len];
-
-            reader.Read(buffer, 0, len);
-
-            return Encoding.ASCII.GetString(buffer.TakeWhile(x => x != 0).ToArray());
-        }
-    }
-
     public class DbHolder
     {
-        public static int RecordsCount;
-
-        Dictionary<object, Func<BinaryReader, object>> propTypeMap = new Dictionary<object, Func<BinaryReader, object>>()
-        {
-            { typeof(int), r => r.ReadInt32() },
-            { typeof(ulong), r => r.ReadUInt64() },
-            { typeof(uint), r => r.ReadUInt32() },
-            { typeof(float), r => r.ReadSingle() },
-            { typeof(IPAddress), r => new IPAddress(r.ReadUInt32()) },
-            { "Name", r => r.ReadFixedString(32) },
-            { "Country", r => r.ReadFixedString(8) },
-            { "Region", r => r.ReadFixedString(12) },
-            { "Postal", r => r.ReadFixedString(12) },
-            { "City", r => r.ReadFixedString(24) },
-            { "Organization", r => r.ReadFixedString(32) }
-        };
-
-
         public void LoadDbFromFile(string path)
         {
-            var bytes = File.ReadAllBytes(path);
+            var fileBuf = File.ReadAllBytes(path);
+            var header = ByteBufToStruct<DbHeader>(fileBuf);
+            var ranges = ByteBufToArrayOfStructs<DbRange>(fileBuf, (int)header.offset_ranges, header.records);
+            var cities = ByteBufToArrayOfStructs<DbCity>(fileBuf, (int)header.offset_cities, header.records);
 
-            var header = new DbHeader(bytes);
+            var locationSize = Marshal.SizeOf<DbLocation>();
 
-            var locations = Enumerable.Range(0, header.Records)
-                                      .Select(i => new DbLocation(bytes, (int)(header.Offset_locations + i * DbLocation.size), DbLocation.size))
-                                      .ToArray();
-
-            var ranges = Enumerable.Range(0, header.Records)
-                                   .Select(i => new DbRange(bytes, (int)(header.Offset_ranges + i * DbRange.size), DbRange.size))
-                                   .ToArray();
-
-            var cities = Enumerable.Range(0, header.Records)
-                                   .Select(i => new DbCity(bytes, (int)(header.Offset_cities + i * 4), 4))
-                                   .ToArray();
+            var locations = Enumerable.Range(0, header.records).Select(i => ByteBufToStruct<DbLocation>(fileBuf, (int)(header.offset_locations + i * locationSize))).ToArray();
         }
 
-        private void SetValue<T>(PropertyInfo p, T obj, BinaryReader reader, DbLocation[] locations = null)
+        private T ByteBufToStruct<T>(byte[] buf, int index = 0) where T : struct
         {
-            Func<BinaryReader, object> valueGetter = null;
-
-            if (p.PropertyType == typeof(DbLocation))
-            {
-                var location = locations[(uint)(propTypeMap[typeof(uint)](reader))];
-
-                p.SetValue(obj, location);
-            }
-            else
-            {
-                if (!propTypeMap.TryGetValue(p.PropertyType, out valueGetter))
-                    valueGetter = propTypeMap[p.Name];
-
-                p.SetValue(obj, valueGetter(reader));
-            }
-        }
-
-        private T ByteBufToObj<T>(byte[] buf, int index = 0, int count = -1, DbLocation[] locations = null) where T : new()
-        {
-            using (var reader = new BinaryReader(new MemoryStream(buf, index, count == -1 ? buf.Length : count)))
-            {
-                var obj = new T();
-
-                obj.GetType().GetProperties().ToList().ForEach(p => SetValue(p, obj, reader, locations));
-
-                return obj;
-            }
-        }
-
-        private T ByteBufToStruct<T>(byte[] buf) where T : struct
-        {
-            if (Marshal.SizeOf(typeof(T)) != buf.Length)
-                throw new ArgumentOutOfRangeException();
-
             var handle = GCHandle.Alloc(buf, GCHandleType.Pinned);
 
             try
             {
-                return Marshal.PtrToStructure<T>(handle.AddrOfPinnedObject());
+                return Marshal.PtrToStructure<T>(handle.AddrOfPinnedObject() + index);
+            }
+            finally
+            {
+                handle.Free();
+            }
+        }
+
+        private T[] ByteBufToArrayOfStructs<T>(byte[] buf, int index, int count) where T: struct
+        {
+            var size = Marshal.SizeOf<T>();
+            var localBuf = new T[count];            
+
+            var handle = GCHandle.Alloc(localBuf, GCHandleType.Pinned);
+
+            try
+            {
+                Marshal.Copy(buf, index, handle.AddrOfPinnedObject(), count * size);
+
+                return localBuf;
             }
             finally
             {
